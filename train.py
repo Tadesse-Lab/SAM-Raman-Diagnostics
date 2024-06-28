447d757c8988d5cafcd295b1f84acb4262141181
jdewbury <Jdewbury@uwaterloo.ca>
2024-06-28 09:46:56 -0400

    Cleanup training code, adjust dataset for ease of use, remove predefined splits

--- train.py
+++ train.py
@@ -6,25 +6,21 @@ import os
 
 from utils.smooth_cross_entropy import smooth_crossentropy
 from utils.log import Log
-from utils.initialize import initialize
+from utils.initialize import get_optimizer
 from utils.step_lr import StepLR
 from utils.bypass_bn import enable_running_stats, disable_running_stats
-from torch import optim
 
 from resnet_1d import ResNet
 from dataset import RamanSpectra
-from sam import SAM
 
 import sys; sys.path.append("..")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', default=200, type=int, help='Total number of epochs.')
-    parser.add_argument('--spectra_dir', default=['data/spectral_data/X_2018clinical.npy','data/spectral_data/X_2019clinical.npy'], type=list, help='Directory to spectra.')
-    parser.add_argument('--label_dir', default=['data/spectral_data/y_2018clinical.npy','data/spectral_data/y_2019clinical.npy'], type=list, help='Directory to labels.')
-    parser.add_argument('--list_trials', default=[1,2,3,4,5,6,7,8,9,10], type=list, help='Specified trials to run.')
-    parser.add_argument('--spectra_interval', default=[400,100], type=list, help = 'Specified patient intervals for clinical significance.')
-    parser.add_argument('--splits_dir', default='utils/splits.npy', type=str, help='Directory to data splits.')
+    parser.add_argument('--spectra_dir', default=['data/spectral_data/X_2018clinical.npy', 'data/spectral_data/X_2019clinical.npy'], type=list, help='Directory to spectra.')
+    parser.add_argument('--label_dir', default=['data/spectral_data/y_2018clinical.npy', 'data/spectral_data/y_2019clinical.npy'], type=list, help='Directory to labels.')
+    parser.add_argument('--spectra_interval', default=[400, 100], type=list, help = 'Specified patient intervals for clinical significance.')
 
     parser.add_argument('--batch_size', default=16, type=int, help='Batch size for the training and validation loops.')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='Learning rate for the optimizer.')
@@ -32,7 +28,7 @@ if __name__ == "__main__":
     parser.add_argument('--layers', default=6, type=int, help='Number of layers in the model.')
     parser.add_argument('--hidden_size', default=100, type=int, help='Size of the hidden layer in the model.')
     parser.add_argument('--block_size', default=2, type=int, help='Block size for the model.')
-    parser.add_argument('--n_classes', default=5, type=int, help='Number of classes in the classification task.')
+    parser.add_argument('--num_classes', default=5, type=int, help='Number of classes in the classification task.')
     parser.add_argument('--input_dim', default=1000, type=int, help='Input dimension for the model.')
     parser.add_argument('--rho', default=0.05, type=float, help='Rho value for the optimizer.')
     parser.add_argument('--momentum', default=0.9, type=float, help='Momentum value for the optimizer.')
@@ -48,10 +44,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     default_args = {action.dest: action.default for action in parser._actions if action.dest != 'help'}
     args_dict = {**default_args, **vars(args)}
-    initialize(args, seed=args.seed)
 
-    splits = np.load(args.splits_dir, allow_pickle=True).tolist()
-    dir = f"{args.optimizer}{(args.optimizer=='SAM' or args.optimizer=='ASAM')*('_'+args.base_optimizer)}"
+    dir = f"{args.optimizer}{(args.optimizer=='SAM' or args.optimizer=='ASAM')*('_'+args.base_optimizer)}_{args.seed}"
 
     if args.save:
         count = 1
@@ -64,194 +58,131 @@ if __name__ == "__main__":
         os.mkdir(unique_dir)
         np.save(f'{unique_dir}/params.npy', args_dict)
 
-    for trial, split in splits.items():
-        if trial not in args.list_trials:
-            continue
+    dataset = RamanSpectra(args.spectra_dir, args.label_dir, args.spectra_interval, args.seed, 
+                            args.shuffle, num_workers=2, batch_size=args.batch_size)
+    log = Log(log_each=10)
 
-        print('\nTrial', trial, '\nSplit:\n', split)
-        if args.save:
-            trial_dir = unique_dir + f'/{trial}'
-            os.mkdir(trial_dir)
+    hidden_sizes = [args.hidden_size] * args.layers
+    num_blocks = [args.block_size] * args.layers
 
-        dataset = RamanSpectra(args.spectra_dir, args.label_dir, args.spectra_interval, split, args.shuffle, num_workers=2, batch_size=args.batch_size)
-        log = Log(log_each=10)
-
-        hidden_sizes = [args.hidden_size] * args.layers
-        num_blocks = [args.block_size] * args.layers
-
-        model = ResNet(hidden_sizes, num_blocks, input_dim=args.input_dim,
-                    in_channels=args.in_channels, n_classes=args.n_classes, activation=args.activation)
-        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-        model = model.to(device)
-        
-        criterion = smooth_crossentropy
-
-        if args.base_optimizer == 'SGD':
-            print('Base Optimizer SGD')
-            base_optimizer = torch.optim.SGD
-        elif args.base_optimizer == 'Adam':
-            print('Base Optimizer Adam')
-            base_optimizer = torch.optim.Adam
-
-        if args.optimizer == 'Adam':
-            print('Optimizer: Adam')
-            optimizer = optim.Adam(model.parameters(), lr=args.learning_rate, betas=(0.5, 0.999))
-        elif args.optimizer == "SGD":
-            print('Optimizer: SGD')
-            optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate)
-        elif args.optimizer == 'SAM':
-            print('Optimizer: SAM')
-            optimizer = SAM(model.parameters(), base_optimizer, rho=args.rho, adaptive=False, lr=args.learning_rate, weight_decay=args.weight_decay)
-        elif args.optimizer == 'ASAM':
-            print('Optimizer: ASAM')
-            optimizer = SAM(model.parameters(), base_optimizer, rho=args.rho, adaptive=True, lr=args.learning_rate, weight_decay=args.weight_decay)
+    model = ResNet(hidden_sizes, num_blocks, input_dim=args.input_dim,
+                in_channels=args.in_channels, num_classes=args.num_classes, activation=args.activation)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
     
-        scheduler = StepLR(optimizer, args.learning_rate, args.epochs)
+    criterion = smooth_crossentropy
 
-        print('Starting Training')
-        
-        train_loss = []
-        train_accuracy = []
-        val_loss = []
-        val_accuracy = []
-        time_epochs = []
-        best_acc = 0
+    optimizer, base_optimizer = get_optimizer(model, args.optimizer, args.learning_rate, args.base_optimizer_name, 
+                                              args.rho, args.weight_decay)
 
-        for epoch in range(args.epochs):
-            start_time = time.time()
-            model.train()
-            log.train(len_dataset=len(dataset.train))
-            batch_loss = []
-            batch_acc = []
+    scheduler = StepLR(optimizer, args.learning_rate, args.epochs)
 
-            for train_index, batch in enumerate(dataset.train):
-                inputs, targets = (b.to(device) for b in batch)
-                inputs = inputs
-                
-                if args.optimizer != 'SAM' and args.optimizer != 'ASAM':
-                    predictions = model(inputs)
-                    targets = targets.to(torch.long)
+    print('Starting Training')
+    
+    train_loss = []
+    train_accuracy = []
+    val_loss = []
+    val_accuracy = []
+    time_epochs = []
+    best_acc = 0
 
-                    loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
+    for epoch in range(args.epochs):
+        start_time = time.time()
+        model.train()
+        log.train(len_dataset=len(dataset.train))
+        batch_loss = []
+        batch_acc = []
 
-                    optimizer.zero_grad()
-                    loss.mean().backward()
-                    optimizer.step()
-                else:
-                    enable_running_stats(model)
-                    predictions = model(inputs)
-                    targiets = targets.to(torch.long)
-                    loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
-                    loss.mean().backward()
-                    optimizer.first_step(zero_grad=True)
+        for train_index, batch in enumerate(dataset.train):
+            inputs, targets = (b.to(device) for b in batch)
+            inputs = inputs
+            
+            if args.optimizer not in ['SAM', 'ASAM']:
+                predictions = model(inputs)
+                targets = targets.to(torch.long)
 
-                    disable_running_stats(model)
-                    smooth_crossentropy(model(inputs), targets, smoothing=args.label_smoothing).mean().backward()
-                    optimizer.second_step(zero_grad=True)
+                loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
 
-                with torch.no_grad():
-                    correct = torch.argmax(predictions.data, 1) == targets
-                    accuracy = correct.float().mean().item()
-                    loss_avg = loss.mean().item()
+                optimizer.zero_grad()
+                loss.mean().backward()
+                optimizer.step()
+            else:
+                enable_running_stats(model)
+                predictions = model(inputs)
+                targiets = targets.to(torch.long)
+                loss = smooth_crossentropy(predictions, targets, smoothing=args.label_smoothing)
+                loss.mean().backward()
+                optimizer.first_step(zero_grad=True)
 
-                    batch_loss.append(loss_avg)
-                    batch_acc.append(accuracy) 
-                    
-                    log(model, loss.cpu(), correct.cpu(), scheduler.lr())
-                    scheduler(epoch)
-
-            epoch_loss_avg = np.mean(batch_loss)
-            epoch_accuracy_avg = np.mean(batch_acc)
-            train_loss.append(epoch_loss_avg)
-            train_accuracy.append(epoch_accuracy_avg)
-
-            model.eval()
-            log.eval(len_dataset=len(dataset.val))
-
-            batch_loss = []
-            batch_acc = []
+                disable_running_stats(model)
+                smooth_crossentropy(model(inputs), targets, smoothing=args.label_smoothing).mean().backward()
+                optimizer.second_step(zero_grad=True)
 
             with torch.no_grad():
-                for batch in dataset.val:
-                    inputs, targets = (b.to(device) for b in batch)
+                correct = torch.argmax(predictions.data, 1) == targets
+                accuracy = correct.float().mean().item()
+                loss_avg = loss.mean().item()
 
-                    predictions = model(inputs)
-                    targets = targets.to(torch.long)
-                    loss = smooth_crossentropy(predictions, targets)
-                    correct = torch.argmax(predictions, 1) == targets
-                    accuracy = correct.float().mean().item()
-                    
-                    try:
-                        loss_avg = loss.mean().item()
-                    except:
-                        loss_avg = 0
+                batch_loss.append(loss_avg)
+                batch_acc.append(accuracy) 
+                
+                log(model, loss.cpu(), correct.cpu(), scheduler.lr())
+                scheduler(epoch)
 
-                    batch_loss.append(loss_avg)
-                    batch_acc.append(accuracy)
+        epoch_loss_avg = np.mean(batch_loss)
+        epoch_accuracy_avg = np.mean(batch_acc)
+        train_loss.append(epoch_loss_avg)
+        train_accuracy.append(epoch_accuracy_avg)
 
-                    log(model, loss.cpu(), correct.cpu())
-
-            epoch_loss_avg = np.mean(batch_loss)
-            epoch_accuracy_avg = np.mean(batch_acc)
-            val_loss.append(epoch_loss_avg)
-            val_accuracy.append(epoch_accuracy_avg)
-
-            end_time = time.time()
-            epoch_time = end_time - start_time
-            time_epochs.append(epoch_time)
-
-            if epoch_accuracy_avg > best_acc and args.save:
-                #print('Saving New Best Val Model')
-                best_val = epoch_accuracy_avg
-                torch.save(model.state_dict(), f'{trial_dir}/best_val.pth')
-            log.flush
-        
-        #print('Starting Testing')
-        if args.save:
-            model.load_state_dict(torch.load(f'{trial_dir}/best_val.pth'))
-        
         model.eval()
+        log.eval(len_dataset=len(dataset.val))
 
         batch_loss = []
         batch_acc = []
 
-        start_time = time.time()
         with torch.no_grad():
-            for batch in dataset.test:
+            for batch in dataset.val:
                 inputs, targets = (b.to(device) for b in batch)
-                inputs = inputs.float()
 
                 predictions = model(inputs)
                 targets = targets.to(torch.long)
                 loss = smooth_crossentropy(predictions, targets)
                 correct = torch.argmax(predictions, 1) == targets
                 accuracy = correct.float().mean().item()
-                loss_avg=loss.mean().item()
+                
+                try:
+                    loss_avg = loss.mean().item()
+                except:
+                    loss_avg = 0
 
                 batch_loss.append(loss_avg)
                 batch_acc.append(accuracy)
 
+                log(model, loss.cpu(), correct.cpu())
+
+        epoch_loss_avg = np.mean(batch_loss)
+        epoch_accuracy_avg = np.mean(batch_acc)
+        val_loss.append(epoch_loss_avg)
+        val_accuracy.append(epoch_accuracy_avg)
+
         end_time = time.time()
-        inference_time = end_time - start_time
+        epoch_time = end_time - start_time
+        time_epochs.append(epoch_time)
 
-        test_loss = np.mean(batch_loss)
-        test_accuracy = np.mean(batch_acc)
+        if epoch_accuracy_avg > best_acc and args.save:
+            best_val = epoch_accuracy_avg
+            torch.save(model.state_dict(), f'{unique_dir}/best_val.pth')
 
-        #print(' ')
-        #print('Test Loss:', epoch_loss_avg, 'Test Accuracy:', epoch_accuracy_avg)
-        log.test(test_loss, test_accuracy)
+        log.flush
 
-        scores = {
-            'train-time': time_epochs,
-            'train-loss': train_loss,
-            'train-acc': train_accuracy,
-            'val-loss': val_loss,
-            'val-acc': val_accuracy,
-            'test-time': inference_time,
-            'test-loss': test_loss,
-            'test-acc': test_accuracy,
-        }
+    scores = {
+        'train-time': time_epochs,
+        'train-loss': train_loss,
+        'train-acc': train_accuracy,
+        'val-loss': val_loss,
+        'val-acc': val_accuracy,
+    }
 
-        if args.save:
-            print(f'Saving values at {trial_dir}')
-            np.save(f'{trial_dir}/scores.npy', scores)
+    if args.save:
+        print(f'Saving values at {unique_dir}')
+        np.save(f'{unique_dir}/scores.npy', scores)
--- utils/initialize.py
+++ utils/initialize.py
@@ -1,13 +1,28 @@
-import random
 import torch
+from sam import SAM
 
+def get_optimizer(model, optimizer_name, learning_rate, base_optimizer_name=None, rho=None, weight_decay=None):
+    base_optimizers = {
+        'SGD': torch.optim.SGD,
+        'Adam': torch.optim.Adam
+    }
+    optimizers = {
+        'Adam': lambda: torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(0.5, 0.999)),
+        'SGD': lambda: torch.optim.SGD(model.parameters(), lr=learning_rate),
+        'SAM': lambda: SAM(model.parameters(), base_optimizer, rho=rho, adaptive=False, lr=learning_rate, weight_decay=weight_decay),
+        'ASAM': lambda: SAM(model.parameters(), base_optimizer, rho=rho, adaptive=True, lr=learning_rate, weight_decay=weight_decay)
+    }
+    if base_optimizer_name:
+        if base_optimizer_name not in base_optimizers:
+            raise ValueError(f'Unsupported base optimizer: {base_optimizer_name}')
+        
+        base_optimizer = base_optimizers[base_optimizer_name]
+        print(f'Base Optimizer: {base_optimizer_name}')
 
-def initialize(args, seed: int):
-    random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    if optimizer_name not in optimizers:
+        raise ValueError(f'Unsupported optimizer: {optimizer_name}')
 
-    torch.backends.cudnn.enabled = True
-    torch.backends.cudnn.benchmark = True
-    torch.backends.cudnn.deterministic = False
+    optimizer = optimizers[optimizer_name]()
+    print(f'Optimizer: {optimizer_name}')
+
+    return optimizer, base_optimizer
